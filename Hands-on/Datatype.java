a533575f6e2ad6f76822150c0d2c86ac3a9780cb
priyasharma1602 <100843812+priyasharma1602@users.noreply.github.com>
2022-05-11 10:55:55 +0530

    Add files via upload

---
+++ Hands-on/Datatype.java
@@ -0,0 +1,23 @@
+package corejava;
+
+public class Datatype {
+	public static void main(String[] args)
+	{
+		int a=50;
+		int b=30;
+		System.out.println("Enter two numbers:"+a +b);
+		
+		  int c=a+b;
+		  int d=a-b;
+		  int e=a*b;
+		  int f=a/b;
+		  int g=a%b;
+		  
+		  System.out.println("The Sum is:"+c);
+		  System.out.println("The Sub is:"+d);
+		  System.out.println("The Mul is:"+e);
+		  System.out.println("The div is:"+f);
+		  System.out.println("The Div is:"+g);
+		 }
+
+}

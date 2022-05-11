66d25b196b0c0bfb03df10cce9d5777a343f037d
priyasharma1602 <100843812+priyasharma1602@users.noreply.github.com>
2022-05-11 12:21:04 +0530

    Add files via upload

---
+++ Hands-on/Table.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Table {
+	    public static void main(String[] args) 
+	    {
+	    	int a=2;
+	    	for(int i=1; i<=10; i++) 
+	    	{
+	    	  System.out.println(a+"*"+i+"="+a*i);	
+	    	}
+	    }
+	    
+}
+	    

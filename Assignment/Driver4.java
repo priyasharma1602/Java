b80ad6668da66bfe657a92350dffc1c32a03b206
priyasharma1602 <100843812+priyasharma1602@users.noreply.github.com>
2022-05-27 17:49:04 +0530

    Add files via upload

---
+++ Assignment/Driver4.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Driver4 
+{
+	public static void main ( String [] args ) 
+	{ 
+	int [] intArr = new int [5]; 
+	for (int i = 0; i<intArr .length ;i ++) 
+	{
+	intArr [ i ] = i * 2;
+	 }
+	 for (int i = 0; i<intArr .length ;i ++) 
+	{ 
+	System .out . print ( intArr [ i ] + ", ") ;
+	 }
+	 } 
+
+}
---
+++ Assignment/Test1.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Test1 
+{
+	public static void main(String[] args)
+	{
+	int arr[] = { 11, 22, 33};
+	for(int i = 0; i<arr.length; i++)
+	System.out.print(arr[i] + " ");
+	System.out.println();
+	int arr2[] = { 11, 22, 33};
+	for(int i = 0; i< arr2.length; i++)
+	System.out.print(arr2[i] + " ");
+	}
+
+}

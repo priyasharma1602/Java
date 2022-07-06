9d100241b0309634a374f303bd87f06c077a56f4
priyasharma1602 <100843812+priyasharma1602@users.noreply.github.com>
2022-07-06 11:55:32 +0530

    Add files via upload

---
+++ Project/BankingSystemApp (1)/BankingSystemApp/BankingSystemApp/src/main/java/com/bankingsystem/exception/GlobalException.java
@@ -0,0 +1,12 @@
+package com.bankingsystem.exception;
+
+public class GlobalException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GlobalException(String message) {
+		super(message);
+	}
+}

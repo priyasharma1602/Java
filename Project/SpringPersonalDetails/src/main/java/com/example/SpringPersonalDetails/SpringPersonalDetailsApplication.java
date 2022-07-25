2b49b0a15dedc9b17a6d2c238aefdc9d6319e1f2
priyasharma1602 <100843812+priyasharma1602@users.noreply.github.com>
2022-07-25 14:31:26 +0530

    Add files via upload

---
+++ Project/SpringPersonalDetails/src/main/java/com/example/SpringPersonalDetails/SpringPersonalDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPersonalDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPersonalDetailsApplication.class, args);
+	}
+
+}
---
+++ Project/SpringPersonalDetails/src/test/java/com/example/SpringPersonalDetails/SpringPersonalDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPersonalDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
